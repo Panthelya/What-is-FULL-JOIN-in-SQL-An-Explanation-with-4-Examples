09bff478bb6cc8658c75ee3c0e7baeb5e517bec5
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-23 17:19:16 +0200

    Add files via upload

---
+++ customer.sql
@@ -0,0 +1,11 @@
+CREATE TABLE customer (
+	id INT PRIMARY KEY,
+	first_name VARCHAR (120),
+	last_name VARCHAR (120)
+
+	
+);
+INSERT INTO customer (id, first_name, last_name) VALUES (1, 'Marvin', 'Simmons');
+INSERT INTO customer (id, first_name, last_name) VALUES (2, 'Marianne', 'Dickens');
+INSERT INTO customer (id, first_name, last_name) VALUES (3, 'Susan', 'Strozzi');
+
