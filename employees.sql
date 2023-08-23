029bb54c1da9414c8737e247b3ff686644226c57
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-23 17:01:39 +0200

    Add files via upload

---
+++ employees.sql
@@ -0,0 +1,13 @@
+CREATE TABLE employees (
+	id INT PRIMARY KEY,
+	first_name VARCHAR (120),
+	last_name VARCHAR (120),
+	department_id INT
+
+	
+);
+INSERT INTO employees (id, first_name, last_name, department_id) VALUES (1, 'Sarah', 'Zimmerman', 2);
+INSERT INTO employees (id, first_name, last_name, department_id) VALUES (2, 'Thomas', 'Tyson', 1);
+INSERT INTO employees (id, first_name, last_name, department_id) VALUES (3, 'Daniel', 'Richardson', 1);
+INSERT INTO employees (id, first_name, last_name, department_id) VALUES (4, 'Sofia', 'Tardelli', 2);
+INSERT INTO employees (id, first_name, last_name, department_id) VALUES (5, 'Mark', 'Fitzpatrick', 4);
