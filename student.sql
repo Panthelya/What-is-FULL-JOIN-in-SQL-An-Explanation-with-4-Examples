27226253d0cee1b01dcdb95e73659f45bbd08eb9
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-23 17:41:36 +0200

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,11 @@
+CREATE TABLE student (
+	id INT PRIMARY KEY,
+	first_name VARCHAR (120),
+	last_name VARCHAR (120)
+
+	
+);
+INSERT INTO student (id, first_name, last_name) VALUES (1, 'Tom', 'Frank');
+INSERT INTO student (id, first_name, last_name) VALUES (2, 'Mary', 'Maddison');
+INSERT INTO student (id, first_name, last_name) VALUES (3, 'Pavel', 'Kuba');
+INSERT INTO student (id, first_name, last_name) VALUES (4, 'Amanda', 'Wilson');
