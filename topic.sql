59102a831621a1d802b2132e8712dc8156c17db1
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-23 18:22:23 +0200

    Add files via upload

---
+++ topic.sql
@@ -0,0 +1,11 @@
+CREATE TABLE topic (
+	id INT PRIMARY KEY,
+	topic_name VARCHAR (120)
+
+	
+);
+INSERT INTO topic (id, topic_name) VALUES (1, 'SQL');
+INSERT INTO topic (id, topic_name) VALUES (2, 'Python');
+INSERT INTO topic (id, topic_name) VALUES (3, 'ML');
+INSERT INTO topic (id, topic_name) VALUES (4, 'SQL Career');
+INSERT INTO topic (id, topic_name) VALUES (5, 'Python Career');
