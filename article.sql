6f6d4431a8aa11476c7f0faae13ba2ab99a456ed
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-23 18:26:19 +0200

    Add files via upload

---
+++ article.sql
@@ -0,0 +1,16 @@
+CREATE TABLE article (
+	id INT PRIMARY KEY,
+	article_title VARCHAR(120),
+	topic_id INT
+	
+);
+INSERT INTO article (id, article_title, topic_id) VALUES (1,'What is FULL JOIN in SQL? An Explanation with 4 Examples', 1);
+INSERT INTO article (id, article_title, topic_id) VALUES (2, 'Pandas in Python', 2);
+INSERT INTO article (id, article_title, topic_id) VALUES (3, 'Supervised Learning', 3);
+INSERT INTO article (id, article_title, topic_id) VALUES (4, 'Basic SQL Interview Questions', 4);
+INSERT INTO article (id, article_title, topic_id) VALUES (5, 'Basic Python Interview Questions', 5);
+INSERT INTO article (id, article_title, topic_id) VALUES (6, 'SQL Window Functions', 1);
+INSERT INTO article (id, article_title, topic_id) VALUES (7, 'Ranking Data in Python', 2);
+INSERT INTO article (id, article_title, topic_id) VALUES (8, 'Unsupervised Learning', 3);
+INSERT INTO article (id, article_title, topic_id) VALUES (9, 'Intermediate SQL Interview Questions', 4);
+INSERT INTO article (id, article_title, topic_id) VALUES (10, 'Intermediate Python Interview Questions', 5);
