a9f85197cc908b8ca6a85fa4090ab4659259a0c8
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-23 16:58:52 +0200

    Add files via upload

---
+++ department.sql
@@ -0,0 +1,11 @@
+CREATE TABLE department (
+	id INT PRIMARY KEY,
+	department_name VARCHAR (120)
+
+	
+);
+INSERT INTO department (id, department_name) VALUES (1, 'IT');
+INSERT INTO department (id, department_name) VALUES (2, 'Accounting');
+INSERT INTO department (id, department_name) VALUES (3, 'Sales');
+
+
