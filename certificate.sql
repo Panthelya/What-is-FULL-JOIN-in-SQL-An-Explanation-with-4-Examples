fc3b344c6492e0f19ad2502ae0ac37efc27a6b43
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-23 17:43:54 +0200

    Add files via upload

---
+++ certificate.sql
@@ -0,0 +1,11 @@
+CREATE TABLE certificate (
+	id INT PRIMARY KEY,
+	certificate_name VARCHAR (120)
+
+	
+);
+INSERT INTO certificate (id, certificate_name) VALUES (1, 'Microsoft Certified: Azure Data Fundamentals');
+INSERT INTO certificate (id, certificate_name) VALUES (2, 'Oracle Database SQL Certified Associate Certification');
+INSERT INTO certificate (id, certificate_name) VALUES (3, 'IBM Certified Database Associate');
+INSERT INTO certificate (id, certificate_name) VALUES (4, 'MySQL 5.7 Database Administrator Certification');
+INSERT INTO certificate (id, certificate_name) VALUES (5, 'EDB PostgreSQL 12 Associate Certification');
