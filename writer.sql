8a4d105c456f57faff70a8a6001d90d8c55a66a6
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-23 18:20:06 +0200

    Add files via upload

---
+++ writer.sql
@@ -0,0 +1,11 @@
+CREATE TABLE writer (
+	id INT PRIMARY KEY,
+	first_name VARCHAR (120),
+	last_name VARCHAR (120)
+
+	
+);
+INSERT INTO writer (id, first_name, last_name) VALUES (1, 'Victoria', 'Thompson');
+INSERT INTO writer (id, first_name, last_name) VALUES (2, 'Mike', 'McGill');
+INSERT INTO writer (id, first_name, last_name) VALUES (3, 'Sky', 'Herrera');
+INSERT INTO writer (id, first_name, last_name) VALUES (4, 'Jimmy', 'Goodman');
