caa851bbf04b09dd3f2f03a779d2f01e88b655b8
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-23 17:50:39 +0200

    Add files via upload

---
+++ certificate_edition.sql
@@ -0,0 +1,16 @@
+CREATE TABLE certificate_edition (
+	id INT PRIMARY KEY,
+	certificate_id INT,
+	edition INT
+	
+);
+INSERT INTO certificate_edition (id, certificate_id, edition) VALUES (1, 1, 2022);
+INSERT INTO certificate_edition (id, certificate_id, edition) VALUES (2, 2, 2022);
+INSERT INTO certificate_edition (id, certificate_id, edition) VALUES (3, 3, 2022);
+INSERT INTO certificate_edition (id, certificate_id, edition) VALUES (4, 4, 2022);
+INSERT INTO certificate_edition (id, certificate_id, edition) VALUES (5, 5, 2022);
+INSERT INTO certificate_edition (id, certificate_id, edition) VALUES (6, 1, 2023);
+INSERT INTO certificate_edition (id, certificate_id, edition) VALUES (7, 2, 2023);
+INSERT INTO certificate_edition (id, certificate_id, edition) VALUES (8, 3, 2023);
+INSERT INTO certificate_edition (id, certificate_id, edition) VALUES (9, 4, 2023);
+INSERT INTO certificate_edition (id, certificate_id, edition) VALUES (10, 5, 2023);
