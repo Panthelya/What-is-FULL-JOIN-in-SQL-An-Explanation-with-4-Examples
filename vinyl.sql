ad4a864fd06461bbc4dc4c8837f34f177a08291a
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-23 17:21:28 +0200

    Add files via upload

---
+++ vinyl.sql
@@ -0,0 +1,12 @@
+CREATE TABLE vinyl (
+	id INT PRIMARY KEY,
+	artist VARCHAR (120),
+	album_name VARCHAR (120),
+	price DECIMAL (10,2)
+
+	
+);
+INSERT INTO vinyl (id, artist, album_name, price) VALUES (1, 'Callier, Terry', 'What Color is Love', 24.99);
+INSERT INTO vinyl (id, artist, album_name, price) VALUES (2, 'Guy, Buddy', 'Sweet Tea', 32.99);
+INSERT INTO vinyl (id, artist, album_name, price) VALUES (3, 'Little Simz', 'A Curious Tale of Trials', 32.99);
+INSERT INTO vinyl (id, artist, album_name, price) VALUES (4, 'LaVette, Bettye', 'Scene of the Crime', 36.99);
