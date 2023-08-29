f3d0727c037363fdc3398ad11491379b90089942
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-29 14:38:55 +0200

    Update article_assignment.sql

--- article_assignment.sql
+++ article_assignment.sql
@@ -5,10 +5,9 @@ CREATE TABLE article_assignment (
 	status VARCHAR (120)
 	
 );
-INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (1, 4, 3, 'Writing' );
-INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (2, 4, 3, 'Revising');
-INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (3, 4, 1, 'Writing');
-INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (4, 3, 5, 'Writing');
-INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (5, 3, 8, 'Writing');
-INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (6, 3, 10, 'Writing');
-INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (7, 1, 7, 'Writing');
+INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (1, 4, 3, 'Revising' );
+INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (2, 4, 1, 'Writing');
+INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (3, 3, 5, 'Writing');
+INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (4, 3, 8, 'Revising');
+INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (5, 3, 10, 'Revising');
+INSERT INTO article_assignment (id, writer_id, article_id, status) VALUES (6, 1, 7, 'Writing');
