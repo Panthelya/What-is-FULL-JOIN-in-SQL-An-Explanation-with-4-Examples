e17f129698304c59ca812c7731c3ae19adf8ab10
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-08-29 14:28:59 +0200

    Update purchase.sql

--- purchase.sql
+++ purchase.sql
@@ -1,16 +1,15 @@
 CREATE TABLE purchase (
 	id INT PRIMARY KEY,
-	purchase_id VARCHAR (120),
 	customer_id INT,
 	vinyl_id INT,
 	purchase_date DATE
 
 	
 );
-INSERT INTO purchase (id, purchase_id, customer_id, vinyl_id, purchase_date) VALUES (1, 'PR_01_2023', 1, 2, '2023_01_03');
-INSERT INTO purchase (id, purchase_id, customer_id, vinyl_id, purchase_date) VALUES (2, 'PR_01_2023', 1, 3, '2023_01_03');
-INSERT INTO purchase (id, purchase_id, customer_id, vinyl_id, purchase_date) VALUES (3, 'PR_02_2023', 1, 2, '2023_01_28');
-INSERT INTO purchase (id, purchase_id, customer_id, vinyl_id, purchase_date) VALUES (4, 'PR_02_2023', 1, 3, '2023_01_28');
-INSERT INTO purchase (id, purchase_id, customer_id, vinyl_id, purchase_date) VALUES (5, 'PR_03_2023', 2, 3, '2023_02_02');
-INSERT INTO purchase (id, purchase_id, customer_id, vinyl_id, purchase_date) VALUES (6, 'PR_03_2023', 2, 2, '2023_02_02');
-INSERT INTO purchase (id, purchase_id, customer_id, vinyl_id, purchase_date) VALUES (7, 'PR_03_2023', 2, 4, '2023_02_02');
+INSERT INTO purchase (id, customer_id, vinyl_id, purchase_date) VALUES (1, 1, 2, '2023_01_03');
+INSERT INTO purchase (id, customer_id, vinyl_id, purchase_date) VALUES (2, 1, 3, '2023_01_12');
+INSERT INTO purchase (id, customer_id, vinyl_id, purchase_date) VALUES (3, 1, 2, '2023_02_18');
+INSERT INTO purchase (id, customer_id, vinyl_id, purchase_date) VALUES (4, 1, 3, '2023_03_01');
+INSERT INTO purchase (id, customer_id, vinyl_id, purchase_date) VALUES (5, 2, 3, '2023_03_01');
+INSERT INTO purchase (id, customer_id, vinyl_id, purchase_date) VALUES (6, 2, 2, '2023_04_01');
+INSERT INTO purchase (id, customer_id, vinyl_id, purchase_date) VALUES (7, 2, 4, '2023_05_01');
